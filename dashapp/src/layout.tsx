bc33d9b19bf5d4f7bb7991700f8ca4a655465c70
jiangq <jiangq@xiaopeng.com>
2024-08-13 23:06:31 +0800

    feat: prolayout ready

---
+++ dashapp/src/layout.tsx
@@ -0,0 +1,14 @@
+import { ProLayout } from "@ant-design/pro-components";
+import { Button } from "antd";
+
+const DashLayout: React.FC = () => {
+  return (
+    <>
+      <ProLayout>
+        <Button type="primary">ant design</Button>
+      </ProLayout>
+    </>
+  );
+};
+
+export default DashLayout;
