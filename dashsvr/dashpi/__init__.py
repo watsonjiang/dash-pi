05aa547eed59d754c43c0e42ccffc09ad9f70257
jiangq <jiangq@xiaopeng.com>
2025-08-19 00:17:14 +0800

    feat: server side session

--- dashsvr/dashpi/__init__.py
+++ dashsvr/dashpi/__init__.py
@@ -1,6 +1,7 @@
 import json
 
 from flask import Flask, send_from_directory
+from flask_session import Session
 
 from .auth import bind_auth
 from .exc import DashException
@@ -20,6 +21,9 @@ def create_app(test_config=None):
     app = Flask(__name__, instance_relative_config=True)
     app.config['SECRET_KEY'] = b"1233221"
     app.config['SESSION_COOKIE_NAME'] = 'dash_session'
+    app.config['SESSION_PERMANENT'] = False
+    app.config['SESSION_TYPE'] = 'filesystem'
+    Session(app)
     bind_auth(app)
     app.register_error_handler(DashException, handle_dash_exc)
 
